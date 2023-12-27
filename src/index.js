1b0c6f9ed6ec6f6b0a4fcd011841cfc7ef5478e2
Adryann Layon <adryann.layon7@gmail.com>
2023-12-27 13:31:59 -0300

    :sparkles: feat: Add error handler

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express'); // import express
+require('express-async-errors');
 
 const routes = require('./routes');
 
 const app = express(); // initializing express
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000')); // listening server
