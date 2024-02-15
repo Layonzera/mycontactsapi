49bc9df75ee12781e6030be2862650e57885befc
Adryann Layon <adryann.layon7@gmail.com>
2024-02-14 21:42:32 -0300

    refactior: change port

--- src/index.js
+++ src/index.js
@@ -11,4 +11,4 @@ app.use((error, request, response, next) => {
   response.sendStatus(500);
 });
 
-app.listen(3000, () => console.log('Server started at http://localhost:3000')); // listening server
+app.listen(3001, () => console.log('Server started at http://localhost:3001')); // listening server
