f0c0539c7b0b2217871fd45f1b6aee9598314be2
Adryann Layon <adryann.layon7@gmail.com>
2024-02-15 21:11:58 -0300

    feat: use middlewares on the server

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ const express = require('express'); // import express
 require('express-async-errors');
 
 const routes = require('./routes');
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 
 const app = express(); // initializing express
 app.use(express.json());
+
+app.use(cors); // middleware de CORS
 app.use(routes);
-app.use((error, request, response, next) => {
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
 app.listen(3001, () => console.log('Server started at http://localhost:3001')); // listening server
